395a65745129da61b8d91fb6676203e75637ba60
02-Hshu <jeff.galaxy02@gmail.com>
2020-07-27 21:41:25 -0700

    Changed Crop_Sort to put cropped images in a specific folder

--- Crop_Sort/CropImages.py
+++ Crop_Sort/CropImages.py
@@ -1,27 +1,40 @@
 import glob
+import os
 import cv2
 import matplotlib.pyplot as plt
 
 # Gets all of the images from a folder and stores it in matfiles. 
-direc = '/home/jeff/Documents/Ultrascan/Ultrascan_Py/Crop_Sort/Test_Images/'
-matfiles = glob.glob(direc + '*')
+# Make sure to change these when selecting a new folder with images to crop 
+direc = '/home/jeff/Documents/Ultrascan/Training_Images/'
+direc_to_images = direc + '/Training_Images/'
+direc_to_cropped_images = direc + '/Cropped_Images/'
+
+# Create cropped images folder
+try:
+    os.mkdir(direc_to_cropped_images)
+except OSError:
+    print("Failed to create cropped images directory or cropped images directory already created")
+
+# Make sure this leads to diretory with images
+matfiles = glob.glob(direc_to_images + '*')
 
 nfiles = len(matfiles)
 
 # Loops through all the images and makes a cropped version
 # that is then written into whereever this file is locoated. 
 for i in range(nfiles):
-    img_name = matfiles[i].replace(direc, '').replace('.bmp', '')
+    # Make sure it replaces the path to the directory with the images
+    img_name = matfiles[i].replace(direc_to_images, '').replace('.bmp', '')
     img_number = int(img_name.split("_")[1])
     
     img = cv2.imread(matfiles[i])
-    crop_img = img[0: 640, 0: 640]
+    crop_img = img[0: 640, 0: 800]
     
     if (img_number < 10):
-        cv2.imwrite('image_000' + str(img_number) + '.bmp', crop_img)
+        cv2.imwrite(direc_to_cropped_images + 'image_000' + str(img_number) + '.bmp', crop_img)
     elif (img_number < 100):
-        cv2.imwrite('image_00' + str(img_number) + '.bmp', crop_img)
+        cv2.imwrite(direc_to_cropped_images + 'image_00' + str(img_number) + '.bmp', crop_img)
     elif (img_number < 1000):
-        cv2.imwrite('image_0' + str(img_number) + '.bmp', crop_img)
+        cv2.imwrite(direc_to_cropped_images + 'image_0' + str(img_number) + '.bmp', crop_img)
     elif (img_number < 10000):
-        cv2.imwrite('image_' + str(img_number) + '.bmp', crop_img)
+        cv2.imwrite(direc_to_cropped_images + 'image_' + str(img_number) + '.bmp', crop_img)
---
+++ Test.py
@@ -0,0 +1,4 @@
+test = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+print(test)
+print("test[1, 0, 0]")
+print(test[1, 0, 0])
